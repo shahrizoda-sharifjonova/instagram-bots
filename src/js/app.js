591b4df0f1964bddd1b411a6b75be550f9168644
shahrizoda <shahrizodadeveloping@gmail.com>
2022-05-09 21:31:26 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,13 @@ functions.isWebp();
 // import Swiper, { Navigation, Pagination } from 'swiper';
 
 // const swiper = new Swiper();
+
+const menu = document.querySelector('.menu');
+const headerContent = document.querySelector('.header__content');
+const body = document.querySelector('body');
+
+menu.addEventListener('click', (e)=>{
+    headerContent.classList.toggle('active')
+    menu.classList.toggle('active')
+    body.classList.toggle('hidden')
+})

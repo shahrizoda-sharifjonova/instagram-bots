df38224062fb3ecedb2c54e00973d3f3e3277355
shahrizoda <shahrizodadeveloping@gmail.com>
2022-05-14 22:17:01 +0500

    end quest block

--- src/js/app.js
+++ src/js/app.js
@@ -26,3 +26,21 @@ reviewsBtn1.addEventListener('click', (e)=>{
         reviewsContent.classList.remove('active')
     })
 })
+
+$(document).ready(function() {
+    $(".accordion > .accordion__button").on("click", function() {
+      if ($(this).hasClass("active")) {
+        $(this).removeClass("active");
+        $(this)
+          .siblings(".accordion__content")
+          .slideUp(200);
+      } else {
+        $(".accordion > .accordion__button").removeClass("active");
+        $(this).addClass("active");
+        $(".accordion__content").slideUp(200);
+        $(this)
+          .siblings(".accordion__content")
+          .slideDown(200);
+      }
+    });
+  });
